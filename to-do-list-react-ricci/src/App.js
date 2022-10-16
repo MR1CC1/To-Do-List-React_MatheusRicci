016d482ffa5e78e2eb2c5d3cb3acd94f88b57416
Matheus Ricci <57235913+MR1CC1@users.noreply.github.com>
2022-10-16 06:29:33 -0300

    styling

--- to-do-list-react-ricci/src/App.js
+++ to-do-list-react-ricci/src/App.js
@@ -1,36 +1,55 @@
 import { useState } from "react";
-
+import "./App.css"
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
 
 function App() {
 
   let [list, setList] = useState([]);
   let [newTask, setNewtask] = useState("");
 
-  function addNewTask() {
+  const addNewTask = () => {
     setList([...list, newTask]);
     setNewtask("");
   }
 
-  function delTask(index) {
+  const delTask = (index) => {
     let arrayTemp = [...list];
     arrayTemp.splice(index, 1)
     setList(arrayTemp)
   }
 
   return (
-    <>
-      <input value={newTask} onChange={value =>
-        setNewtask(value.target.value)} type="text" placeholder="Tarefa" />
-      <button onClick={() => addNewTask()}>Add</button>
+    <div className="container">
+
+      {/* <input value={newTask} onChange={inputValue => setNewtask(inputValue.target.value)} type="text" placeholder="Insira sua Tarefa..." /> */}
+      {/* <button onClick={() => (newTask === "") ? alert("Insira uma Tarefa!") : addNewTask()}>Add</button> */}
+      <h1>To Do List - React - Matheus Ricci</h1>
+      <div className="task">
+        <TextField className="input" id="standard-basic" label="Tarefa" variant="standard" value={newTask} onChange={inputValue => setNewtask(inputValue.target.value)} type="text" placeholder="Insira sua Tarefa..." />
+        <Button className="btn-add" onClick={() => (newTask === "") ? alert("Insira uma Tarefa!") : addNewTask()} variant="outlined">Add</Button>
+      </div>
+
       <ul>
         {list.map((task, index) => (
-          <li>
-            {task}
-            <button onClick={() => delTask(index)}>X</button>
-          </li>
+          <div className="divTask">
+            <li className="li-div">
+              {/* <input className="checker" type="checkbox" name="check" id="check" /> */}
+              <Checkbox />
+              <div className="task-div">
+                <p>
+                  {task}
+                </p>
+              </div>
+              {/* <button className="btn-del" onClick={() => delTask(index)}>X</button> */}
+            </li>
+            <Button variant="outlined" color="error" className="btn-del" onClick={() => delTask(index)}>X</Button>
+          </div>
         ))}
       </ul>
-    </>
+    </div>
   );
 }
 
